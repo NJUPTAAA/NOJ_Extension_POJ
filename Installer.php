78dd5e3a187a5c6af5bf5d416e6d9ddaa593d906
ZsgsDesign <zsgsdesign@gmail.com>
2019-07-18 16:19:54 +0800

    installer supported

---
+++ Installer.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Babel\Extension\poj;
+use App\Babel\Install\InstallerBase;
+use Exception;
+class Installer extends InstallerBase
+{
+    public $ocode="poj";
+    public function install()
+    {
+        // throw new Exception("No Install Method Provided");
+        $this->_install($this->ocode);
+    }
+    public function uninstall()
+    {
+        // throw new Exception("No Uninstall Method Provided");
+        $this->_uninstall($this->ocode);
+    }
+}
